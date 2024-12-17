356c37f7fabd9b81899c8f9e7ffcae37dad13bdc
jgab97 <gaboryjordan@gmail.com>
2024-12-16 22:39:13 -0400

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w dero1qyykp0tm77en28a5a94wu9k2vme685mzfugc9p82f366kkpfnd3vsqqjkafug -r dero-node-yashnik-eu.mysrv.cloud:10300 -m 8 -p rpc;
+    ./astrominer -w dero1qyykp0tm77en28a5a94wu9k2vme685mzfugc9p82f366kkpfnd3vsqqjkafug -r dero-node-yashnik-eu.mysrv.cloud:10300 -m 20 -p rpc;
     sleep 5;
 done
